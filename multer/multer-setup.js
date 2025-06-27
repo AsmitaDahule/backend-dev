ad50e4a14f01d5e1beeb95b9d39ca70580cdf78a
Asmita Dahule <dahuleasmita@gmail.com>
2025-06-27 10:36:27 +0530

    limit size

--- multer/multer-setup.js
+++ multer/multer-setup.js
@@ -28,6 +28,10 @@ function fileFilter(req, file, cb) {
 //   }
 // })
 
-const upload = multer({storage: storage, fileFilter: fileFilter});
+const upload = multer({storage: storage, 
+fileFilter: fileFilter,
+limits: {fileSize: 2 * 1024 * 1024}
+});
+
 module.exports = upload;
 
